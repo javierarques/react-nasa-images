12def37f539bd96f7e28310a4ee27d82b3454274
Javi Arques <javier.arques@flywire.com>
2018-01-10 01:27:05 +0100

    Add Home component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,4 +1,20 @@
 import React from 'react';
-const Home = () => <h1>HOME PAGE</h1>;
+import SearchBox from '../SearchBox';
+import './Home.css';
+import nasaLogo from '../nasa-logo.svg';
+
+const getRandom = max => Math.floor(Math.random() * max) + 1;
+
+const Home = () => (
+  <div className={`Home Home--bg${getRandom(10)}`}>
+    <div className="Home-content">
+      <div className="Home-logo">
+        <img src={nasaLogo} alt="NASA logo" width="80" />
+        <h1 className="Home-title">media library</h1>
+      </div>
+      <SearchBox />
+    </div>
+  </div>
+);
 
 export default Home;
---
+++ src/Home/Home.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SearchBox from '../SearchBox';
+import Home from './Home';
+
+describe('Home Component', () => {
+  test('has a SearchBox', () => {
+    const wrapper = shallow(<Home />);
+
+    expect(wrapper.find(SearchBox).length).toEqual(1);
+  });
+});
