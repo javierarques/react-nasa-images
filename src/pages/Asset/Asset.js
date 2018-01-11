0bbf4ee8f7d1d450d5e4a204fe10317344265515
Javi Arques <javier.arques@flywire.com>
2018-01-11 23:25:56 +0100

    Add search box to asset page

--- src/pages/Asset/Asset.js
+++ src/pages/Asset/Asset.js
@@ -1,4 +1,24 @@
 import React from 'react';
-const Asset = () => <h1>ASSET PAGE</h1>;
+import Header from '../../components/Header';
+import SearchBox from '../../components/SearchBox';
+
+class Asset extends React.Component {
+  handleSubmit = query => {
+    const { history } = this.props;
+    history.push(`/search/${query}`);
+  };
+
+  render() {
+    return (
+      <div className="Asset">
+        <div className="Asset-header">
+          <Header>
+            <SearchBox shadow={false} onSubmit={this.handleSubmit} />
+          </Header>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Asset;
