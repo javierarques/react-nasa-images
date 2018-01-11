df06a636ab825ac68f86d8c37afe29c86b48760d
Javi Arques <javier.arques@flywire.com>
2018-01-12 00:38:34 +0100

    Add asset search to api service

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -2,8 +2,10 @@ import fetchJson from '../../utils/fetchJson';
 import { NASA_API_URL } from './constants';
 
 const api = {
-  search: query =>
-    fetchJson(`${NASA_API_URL}/search?q=${query}&media_type=image`)
+  search: query => {
+    return fetchJson(`${NASA_API_URL}/search?q=${query}&media_type=image`);
+  },
+  asset: id => fetchJson(`${NASA_API_URL}/search?nasa_id=${id}`)
 };
 
 export default api;
--- src/services/api/api.test.js
+++ src/services/api/api.test.js
@@ -7,8 +7,10 @@ describe('NASA API', () => {
     const query = 'pluto';
     const dummyFetchResponse = {
       collection: {
-        href: 'https://images-api.nasa.gov/search?q=pluto',
-        items: []
+        version: '1.0',
+        items: [],
+        metadata: {},
+        href: 'https://images-api.nasa.gov/search?q=pluto'
       }
     };
 
@@ -24,6 +26,34 @@ describe('NASA API', () => {
     api.search(query).then(response => {
       expect(response.collection.href).toEqual(
         'https://images-api.nasa.gov/search?q=pluto'
+      );
+      done();
+    });
+  });
+
+  test('asset', done => {
+    const nasa_id = 'PIA11217';
+    const dummyFetchResponse = {
+      collection: {
+        version: '1.0',
+        items: [],
+        metadata: {},
+        href: 'https://images-api.nasa.gov/search?nasa_id=PIA11217'
+      }
+    };
+
+    const url = `${NASA_API_URL}/search?nasa_id=${nasa_id}`;
+    const config = {
+      matcher: url,
+      response: dummyFetchResponse,
+      method: 'GET'
+    };
+
+    fetchMock.mock(config);
+
+    api.asset(nasa_id).then(response => {
+      expect(response.collection.href).toEqual(
+        'https://images-api.nasa.gov/search?nasa_id=PIA11217'
       );
       done();
     });
