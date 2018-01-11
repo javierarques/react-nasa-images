0335558934cbc227658a30f1533db2ad34b974d2
Javi Arques <javier.arques@flywire.com>
2018-01-11 19:59:32 +0100

    Add lazy loading for gallery items

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -2,7 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Gallery.css';
 
-const Gallery = ({ children }) => <div className="Gallery">{children}</div>;
+const Gallery = ({ children }) => {
+  const galleryItems = children.map(child => (
+    <div className="Gallery-item">{child}</div>
+  ));
+
+  return <div className="Gallery">{galleryItems}</div>;
+};
 
 Gallery.propTypes = {
   children: PropTypes.node
--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import LazyLoad from 'react-lazyload';
 import './GalleryItem.css';
 
 const GalleryItem = ({ image, id, title }) => (
   <article className="GalleryItem">
     <Link className="GalleryItem-wrapper" to={`/asset/${id}`} title={title}>
-      <img
-        src={image}
-        alt={title}
-        title={title}
-        className="GalleryItem-image"
-      />
+      <div className="GalleryItem-imageWrapper">
+        <LazyLoad height={200} once>
+          <img
+            src={image}
+            alt={title}
+            title={title}
+            className="GalleryItem-image"
+          />
+        </LazyLoad>
+      </div>
       {title && <p className="GalleryItem-title">{title}</p>}
     </Link>
   </article>
